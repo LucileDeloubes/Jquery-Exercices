cf1299fbbb2386dbaee29b669785d54bb9fc28b0
LucileDeloubes <lucile.deloubes@gmail.com>
2020-11-06 11:40:55 +0100

    Jquery Part 2 exo 4 changer la couleur en appuyant sur un bouton

---
+++ Partie2/Exercice4/assets/js/script.js
@@ -0,0 +1,11 @@
+// consigne : Au clic sur un bouton de couleur, modifier la couleur du texte.
+
+$('#green').click(function textGreen(){
+     $('#text').css('color','green');
+});
+$('#red').click(function textGreen(){
+    $('#text').css('color','red');
+});
+$('#blue').click(function textGreen(){
+    $('#text').css('color','blue');
+});  
