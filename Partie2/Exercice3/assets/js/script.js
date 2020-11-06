ed8915513118729cf5cfe6afc1383a7122a73f8e
LucileDeloubes <lucile.deloubes@gmail.com>
2020-11-06 11:31:58 +0100

    Jquery Part 2 exo 3 afficher / masquer au clic

---
+++ Partie2/Exercice3/assets/js/script.js
@@ -0,0 +1,8 @@
+$("#show").click(function showText(){
+    $('#texte').show();
+});
+$("#hide").click(function hideText(){
+    $('#texte').hide();
+});
+
+// consigne : Afficher ou masquer le paragraphe text au clic des boutons.
