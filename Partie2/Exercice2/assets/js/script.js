c7ced847ef3454140994eead30c476eff0beb05d
LucileDeloubes <lucile.deloubes@gmail.com>
2020-11-06 11:25:04 +0100

    Jquery part 2 exo 2 envoyer alerte

---
+++ Partie2/Exercice2/assets/js/script.js
@@ -0,0 +1,4 @@
+// Au clic sur le bouton, afficher une boîte de dialogue avec le message de votre choix.
+$('#clickMe').click(function sendMessage(){
+    alert('Bravo vous savez appuyer sur un bouton ! ')
+})
