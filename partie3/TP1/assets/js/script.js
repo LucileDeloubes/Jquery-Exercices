afd31272a342507995cf1b29ff73e228c60b31c5
LucileDeloubes <lucile.deloubes@gmail.com>
2020-11-09 15:22:47 +0100

    ajout de commentaires

--- partie3/TP1/assets/js/script.js
+++ partie3/TP1/assets/js/script.js
@@ -1,30 +1,30 @@
 $(function(){
-
+   // générer nombre aléatoire à chaque rechargement de la page
    var randomnumber = Math.floor(Math.random() * 100) + 1;
    alert('Le nombre mystère généré, vous pouvez jouer ! ')
 
-   var count = 0;
+   var count = 0; // Définir un compteur pour le nombre d'essai et lui indiquer qu'à chaque chargement de la page qu'il est à 0.
   
+   // En cliquant sur le bouton "joue"
+   $('#play').click(function compare(){ 
 
-   $('#play').click(function compare(){
-
-      count++;
+      count++; // le compteur prendra +1 à chaque clique
       
-      var playerChoice = $('#number').val();
+      var playerChoice = $('#number').val(); // on récupérer la valeur que le joueur donne dans le input
       
-      if (playerChoice > randomnumber){
+      if (playerChoice > randomnumber){ //Si la valeur est plus grande que le nombre aléatoire
          alert('C\'est moins ! ' );
       }
-      if (playerChoice < randomnumber){
+      if (playerChoice < randomnumber){//Si la valeur est plus petite que le nombre aléatoire
          alert('C\'est plus ! ');
       }
       if (playerChoice == randomnumber){
-         alert('C\'est gagné ! \n'+ 'Vous avez trouvé en ' + count + '  '+ 'tentatives');
+         alert('C\'est gagné ! \n'+ 'Vous avez trouvé en ' + count + '  '+ 'tentatives'); // alerte final et annoncé du nombre de tentative avec count
       } 
    });
  $('#reset').click(function(){
    location.reload();
- });
+ }); //relancer la partie
 });   
 
 
